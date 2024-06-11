d5de83498ec3d4ac4a46db9755ea511907105837
Brian Martin <62304138+bsmartin-ep@users.noreply.github.com>
2024-06-11 10:27:33 -0400

    Update 1.2_identity_graph.sql

--- session3/1.2_identity_graph.sql
+++ session3/1.2_identity_graph.sql
@@ -10,3 +10,20 @@ SELECT
 FROM `df-warehouse.df_warehouse_extras.sample_stg_ga4_events`
 GROUP BY 1
 --HAVING shared_users > 1 -- Remove comment to view devices shared by multiple users
+
+
+-- -- Alternate version with no ARRAY_AGG
+-- WITH user_ids AS (
+--     SELECT
+--         user_pseudo_id,
+--         user_id,
+--         max(event_timestamp) as max_event_timestamp
+--     FROM `df-warehouse.df_warehouse_extras.sample_stg_ga4_events`
+--     GROUP BY 1, 2
+-- )
+
+-- SELECT
+--     user_pseudo_id AS ig_user_pseudo_id,
+--     user_id AS primary_user_id,
+-- FROM user_ids
+-- QUALIFY RANK() OVER (PARTITION BY user_pseudo_id ORDER BY max_event_timestamp DESC) = 1
